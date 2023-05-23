d4e7fafb2fe70077f53190b3b3a722655af3f02d
Gürhan Arslan <35gurhan.arslan35@gmail.com>
2023-05-24 01:13:06 +0300

    get-folder-size-by-glob

---
+++ index.js
@@ -0,0 +1,22 @@
+const glob = require('glob');
+const fs = require('fs');
+
+function getFolderSizeByGlob(folder, { ignorePattern: array }) {
+    const filePaths = glob.sync('**', { // "**" means you search on the whole folder
+        cwd: folder, // folder path 
+        ignore: array, // array of glob pattern strings
+        absolute: true, // you have to set glob to return absolute path not only file names
+    });
+    let totalSize = 0;
+    filePaths.forEach((file) => {
+        console.log('file', file);
+        const stat = fs.statSync(file);
+        totalSize += stat.size;
+    });
+    return totalSize;
+}
+
+
+const response = getFolderSizeByGlob('./public', { ignorePattern: [] })
+
+console.log(response);
